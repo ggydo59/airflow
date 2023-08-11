6af3cfafd833a224d2ad34e4f44db588dcb10d8b
BTP0020-PC\mgd81 <mgd813@bigtech.co.kr>
2023-08-11 18:50:15 +0900

    pythonOperator op args

--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -1,2 +1,11 @@
 def get_sftp():
     print('sftp 작업시작')
+    
+def regist(name, sex, *args):
+    print(f"name = {name}")
+    print(f"sex = {sex}")
+    
+    country = args[0] if len(args) >=1 else None
+    city = args[1] if len(args) >=1 else None
+    print(f"country = {country}")
+    print(f"city={city}")
